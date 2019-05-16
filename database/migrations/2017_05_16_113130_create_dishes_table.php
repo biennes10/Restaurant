966d63c26453f474b958e6e520d2309fba90b7d6
biennes10 <adrien.senut@gmail.com>
2019-05-16 13:57:19 +0200

    migrations

---
+++ database/migrations/2017_05_16_113130_create_dishes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->bigInteger('categorie_id')->unsigned();
+            $table->foreign('categorie_id')->references('id')->on('categories');
+            $table->integer('cost');
+            $table->string('dish');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
